46bf5222f7a3c13b40109f0a85b11cc942283a3c
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-06-23 10:19:36 +0200

    Updgrade dataSource script add ressources folder and ArticleDetailCOntroller

---
+++ Mambo/Controllers/ArticleDetailController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mambo.Controllers
+{
+    public class ArticleDetailController : Controller
+    {
+        // GET: ArticleDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        
+    }
+}
